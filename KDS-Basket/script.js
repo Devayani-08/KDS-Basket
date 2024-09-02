9bce2dd0292dae1cde8a6ff7f37cf2eb8ca5f02c
Devayani <devayani.k@yahoo.com>
2024-09-02 16:17:20 +0530

    changed in about

---
+++ KDS-Basket/script.js
@@ -0,0 +1,4 @@
+var firstName=prompt("enter your first name");
+var LastName=prompt("enter your last name");
+var email=prompt("enter your Email-ID");
+var mobile=prompt("enter your mobile number");
