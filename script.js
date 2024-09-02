7d5bcca1c8745519bdcdc8a15eb11ea079782022
Devayani <devayani.k@yahoo.com>
2024-09-02 23:21:43 +0530

    Extra files deleted

--- script.js
+++
@@ -1,4 +0,0 @@
-var firstName=prompt("enter your first name");
-var LastName=prompt("enter your last name");
-var email=prompt("enter your Email-ID");
-var mobile=prompt("enter your mobile number");
